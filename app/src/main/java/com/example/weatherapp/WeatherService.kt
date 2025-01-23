a20a324370de06eb630b1aeb56f2e7cf404663b6
unknown <a042256@umaia.pt>
2025-01-23 15:17:28 +0000

    update

--- app/src/main/java/com/example/weatherapp/WeatherService.kt
+++ app/src/main/java/com/example/weatherapp/WeatherService.kt
@@ -7,6 +7,6 @@ interface WeatherService {
     @GET("data/2.5/weather")
     suspend fun getWeather(
         @Query("q") city: String,
-        @Query("appid") apiKey: String
+        @Query("721d7638793f057d376a24399df55ec4") apiKey: String
     ): WeatherResponse
 }
